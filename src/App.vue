2424ba3258107569760e93bb016cd854a0ee4bb4
Maximilian Rahn <max.rahn@tum.de>
2019-11-04 13:11:23 +0100

    added scrollama

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Scrollama :progress=true @step-enter="stepEnterHandler" @step-progress="stepProgressHandler">
+      <div class="step1 section" data-step="a" v-bind:style="{fontSize:fontSize_a + 'px'}">
+        <div class='flex-item'>Some arbitrary text</div>
+      </div>
+      <div class="step2 section" data-step="b" v-bind:style="{fontSize:fontSize_a + 'px'}">
+        <div class='flex-item'>Some arbitrary text</div>
+      </div>
+      <div class="step3 section" data-step="c" v-bind:style="{fontSize:fontSize_a + 'px'}">...</div>
+    </Scrollama>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "intersection-observer"; // for cross-browser support
+import Scrollama from "vue-scrollama";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Scrollama
+  },
+  data(){
+    return {
+      styles: [{}]
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    fSize: function(index){
+      return this.fontSize[index]
+    },
+    stepEnterHandler({element, index, direction}) {
+      console.log(element, index, direction);
+    },
+    stepProgressHandler({element, index, progress}){
+      this.styles[index].fontSize = Math.max(10, progress*50)
+      this.fontSize_a = Math.max(10, progress*20)
+      console.log(this.fontSize[index])
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.section {
+  width:99%;
+  height:80vh;
+  display: flex;
+  align-items: center; /* Vertical center alignment */
+  justify-content: center; /* Horizontal center alignment */
+  border: 1px dashed black; 
+  margin-bottom:20px;
+}
+
 </style>
